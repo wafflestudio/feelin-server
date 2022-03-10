d04ae3cf6c6e70235d7ce3bbf903f89a2d64ddbc
moonpiderman <lotsofchuck@gmail.com>
2022-03-10 18:44:37 +0900

    Config: Jwt Authentication filter

--- src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtAuthenticationFilter.kt
@@ -1,8 +1,26 @@
 package com.wafflestudio.msns.global.auth.jwt
 
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 class JwtAuthenticationFilter(
     authenticationManager: AuthenticationManager?,
-
-)
+    private val jwtTokenProvider: JwtTokenProvider,
+) : BasicAuthenticationFilter(authenticationManager) {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        chain: FilterChain
+    ) {
+        val token = request.getHeader(jwtTokenProvider.headerString)
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            val authentication = jwtTokenProvider.getAuthenticationTokenFromJwt(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        chain.doFilter(request, response)
+    }
+}
