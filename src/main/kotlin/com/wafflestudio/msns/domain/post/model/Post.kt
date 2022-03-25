12635d3a7a28d7aa4546d34436f70db292a7058d
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-03-25 12:30:10 +0900

    fix: linter Wildcard import

--- src/main/kotlin/com/wafflestudio/msns/domain/post/model/Post.kt
+++ src/main/kotlin/com/wafflestudio/msns/domain/post/model/Post.kt
@@ -3,7 +3,13 @@ package com.wafflestudio.msns.domain.post.model
 import com.wafflestudio.msns.domain.model.BaseTimeEntity
 import com.wafflestudio.msns.domain.playlist.model.Playlist
 import com.wafflestudio.msns.domain.user.model.User
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
 
 @Entity
 @Table(
--- src/main/kotlin/com/wafflestudio/msns/domain/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/msns/domain/user/api/UserController.kt
@@ -7,7 +7,11 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
 
 @RestController
 @RequestMapping("/api/v1/user")
