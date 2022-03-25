af198f3bf9579e9fb306d104eb200f8b52e9a20b
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-03-25 12:34:32 +0900

    fix: linter Wildcard import

--- src/main/kotlin/com/wafflestudio/msns/domain/post/api/PostController.kt
+++ src/main/kotlin/com/wafflestudio/msns/domain/post/api/PostController.kt
@@ -6,7 +6,15 @@ import com.wafflestudio.msns.domain.post.service.PostService
 import com.wafflestudio.msns.domain.user.model.User
 import com.wafflestudio.msns.global.auth.CurrentUser
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 import javax.validation.Valid
 
 @RestController
