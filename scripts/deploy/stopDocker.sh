205e3145ca18d1bde17772f45bfdbd1f8f98ab17
yeonsumia <yeonsumia@snu.ac.kr>
2022-09-17 14:04:38 +0900

    chore: .sh files

--- scripts/deploy/stopDocker.sh
+++ scripts/deploy/stopDocker.sh
@@ -2,8 +2,6 @@
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-    # Remove any anonymous volumes attached to containers (-v)
     sudo docker-compose -f ~/deploy/docker-compose.yml down -v
-    # build images and run containers
     echo "[Deploy] : Stopping previous Application (dev)"
 fi
