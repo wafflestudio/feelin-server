d8bf105753c6e7612113dd2fa7bb44c80fc89246
Hyeonseok Jung <hsjames0248@gmail.com>
2023-01-07 21:44:56 +0900

    feat: print jwt secret

--- src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtTokenProvider.kt
@@ -21,24 +21,20 @@ import java.security.SignatureException
 import java.time.Duration
 import java.util.Date
 import java.util.UUID
+import javax.annotation.PostConstruct
 
 @Component
 class JwtTokenProvider(
+    @Value("\${spring.jwt.secret}") private val jwtSecretKey: String,
     private val userRepository: UserRepository,
     private val verificationTokenRepository: VerificationTokenRepository,
 ) {
+    @PostConstruct
+    fun init() {
+        System.out.println("jwtSecretKey: $jwtSecretKey")
+    }
+
     private val logger = LoggerFactory.getLogger(JwtTokenProvider::class.java)
-    val tokenPrefix = "Bearer "
-    val headerString = "Authentication"
-
-    @Value("\${spring.jwt.secret}")
-    val jwtSecretKey: String = ""
-
-    private val jwtJoinExpirationInMs: Long = Duration.ofMinutes(10).toMillis()
-
-    private val jwtExpirationInMs: Long = Duration.ofHours(1).toMillis()
-
-    private val jwtRefreshExpirationInMs: Long = Duration.ofDays(14).toMillis()
 
     fun generateToken(id: UUID, type: JWT): String {
         val claims: MutableMap<String, Any> = hashMapOf("id" to id.toString())
@@ -128,5 +124,13 @@ class JwtTokenProvider(
         val authorities = userPrincipal.authorities
 
         return AuthenticationToken(userPrincipal, null, authorities)
+    }
+
+    companion object {
+        private val tokenPrefix = "Bearer "
+        private val headerString = "Authentication"
+        private val jwtJoinExpirationInMs: Long = Duration.ofMinutes(10).toMillis()
+        private val jwtExpirationInMs: Long = Duration.ofHours(1).toMillis()
+        private val jwtRefreshExpirationInMs: Long = Duration.ofDays(14).toMillis()
     }
 }
