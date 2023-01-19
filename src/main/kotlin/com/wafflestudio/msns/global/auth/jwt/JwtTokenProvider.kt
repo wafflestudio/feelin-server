0a81d460522fa754c32384e60611dc3f4e3de1ca
Hyeonseok Jung <hsjames0248@gmail.com>
2023-01-20 01:31:42 +0900

    refactor: Refactor JwtTokenProvider

--- src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/com/wafflestudio/msns/global/auth/jwt/JwtTokenProvider.kt
@@ -24,15 +24,13 @@ import java.util.UUID
 
 @Component
 class JwtTokenProvider(
+    @Value("\${spring.jwt.secret}") private val jwtSecretKey: String,
     private val userRepository: UserRepository,
     private val verificationTokenRepository: VerificationTokenRepository,
 ) {
-    private val logger = LoggerFactory.getLogger(JwtTokenProvider::class.java)
     val tokenPrefix = "Bearer "
     val headerString = "Authentication"
-
-    @Value("\${spring.jwt.secret}")
-    val jwtSecretKey: String = ""
+    private val logger = LoggerFactory.getLogger(JwtTokenProvider::class.java)
 
     private val jwtJoinExpirationInMs: Long = Duration.ofMinutes(10).toMillis()
 
@@ -128,5 +126,11 @@ class JwtTokenProvider(
         val authorities = userPrincipal.authorities
 
         return AuthenticationToken(userPrincipal, null, authorities)
+    }
+
+    companion object {
+        private val jwtJoinExpirationInMs: Long = Duration.ofMinutes(10).toMillis()
+        private val jwtExpirationInMs: Long = Duration.ofHours(1).toMillis()
+        private val jwtRefreshExpirationInMs: Long = Duration.ofDays(14).toMillis()
     }
 }
