0aa702e3ff0799f100ae67f4274086a514131988
yeonsumia <yeonsumia@snu.ac.kr>
2023-01-27 18:05:12 +0900

    refactor: flyway user defined function

--- src/main/resources/db/migration/V1_5__fix_playlist_playlist_id_uuid_type.sql
+++ src/main/resources/db/migration/V1_5__fix_playlist_playlist_id_uuid_type.sql
@@ -1,12 +1,9 @@
-CREATE FUNCTION BIN_TO_UUID(bin BINARY(16))
-    RETURNS CHAR(36) DETERMINISTIC
-BEGIN
-    DECLARE hex CHAR(32);
-    SET hex = HEX(bin);
-    RETURN LOWER(CONCAT(LEFT(hex, 8), '-', MID(hex, 9, 4), '-', MID(hex, 13, 4), '-', MID(hex, 17, 4), '-', RIGHT(hex, 12)));
-END;
-
 alter table playlist add column playlist_id_tmp CHAR(36);
-update playlist set playlist_id_tmp = BIN_TO_UUID(playlist_id);
+
+update playlist
+set playlist_id_tmp = LOWER(CONCAT(LEFT(HEX(playlist_id), 8), '-', MID(HEX(playlist_id), 9, 4), '-',
+                                   MID(HEX(playlist_id), 13, 4), '-', MID(HEX(playlist_id), 17, 4), '-',
+                                   RIGHT(HEX(playlist_id), 12)));
+
 alter table playlist drop column playlist_id;
 alter table playlist rename column playlist_id_tmp TO playlist_id;
