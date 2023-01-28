6cb301fd31adcec1a14758f05389b0cec01c738c
yeonsumia <yeonsumia@snu.ac.kr>
2023-01-28 23:23:59 +0900

    hotfix

--- src/main/kotlin/com/wafflestudio/msns/global/auth/service/AuthService.kt
+++ src/main/kotlin/com/wafflestudio/msns/global/auth/service/AuthService.kt
@@ -32,7 +32,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
