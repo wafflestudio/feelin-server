9ef13131fcd1e5a257fcff185089e3c2529aa0a6
yeonsumia <yeonsumia@snu.ac.kr>
2023-01-30 23:50:31 +0900

    fix: post main track possible less than three

--- src/main/kotlin/com/wafflestudio/msns/domain/post/model/Post.kt
+++ src/main/kotlin/com/wafflestudio/msns/domain/post/model/Post.kt
@@ -53,7 +53,7 @@ class Post(
 
 @Embeddable
 class PostMainTrack(
-    val title: String?,
-    val artist: String?,
-    val thumbnail: String?
+    val title: String,
+    val artist: String,
+    val thumbnail: String
 )
