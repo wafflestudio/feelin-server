938f22edf8f4b530dfbe0317c65a9f0aecef131c
yeonsumia <yeonsumia@snu.ac.kr>
2023-02-08 06:37:13 +0900

    fix: sms content including app Hash and formatting

--- src/main/kotlin/com/wafflestudio/msns/global/sms/service/SMSService.kt
+++ src/main/kotlin/com/wafflestudio/msns/global/sms/service/SMSService.kt
@@ -2,16 +2,18 @@ package com.wafflestudio.msns.global.sms.service
 
 import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sns.model.PublishRequest
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class SMSService(
-    private val amazonSNS: AmazonSNS
+    private val amazonSNS: AmazonSNS,
+    @Value("\${spring.sms.app-hash}") private val appHash: String,
 ) {
     suspend fun sendSMS(countryCode: String, phoneNumber: String, code: String) {
         val publishRequest = PublishRequest()
         publishRequest.phoneNumber = countryCode + phoneNumber.replace("-", "")
-        publishRequest.message = code
+        publishRequest.message = "[Feelin’] $code is your verification code.$appHash"
         amazonSNS.publish(publishRequest)
     }
 }
